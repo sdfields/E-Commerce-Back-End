dc40b8e1f8537ca8acaf52410408f074807f3f42
sdfields <spencerdfields@gmail.com>
2023-03-02 12:45:56 -0600

    added get route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,22 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product,
+        attribute: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+  res.status(200).json(categoryData);
+} catch(err) {
+  res.status(500).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
