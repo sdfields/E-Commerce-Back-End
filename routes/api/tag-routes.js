8d7b6bf3cccccbb3d57947607a97461ed4dea69e
sdfields <spencerdfields@gmail.com>
2023-03-03 22:33:23 -0600

    last updates to route testing

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagsData = await Tag.findOneByPk(req.params.id, {
+    const tagsData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Product data
       include: { 
         model: Product,
