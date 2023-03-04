10f7dc63576264991303e2ae235f1d390fae5f14
sdfields <spencerdfields@gmail.com>
2023-03-03 22:28:33 -0600

    added product tags routes and started testing

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const tagsData = await Tags.findAll({
+    const tagsData = await Tag.findAll({
       // be sure to include its associated Product data
       include: {
         model: Product,
@@ -17,26 +17,73 @@ router.get('/', async (req, res) => {
     }
     res.status(200).json(tagsData);
   } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  try {
+    const tagsData = await Tag.findOneByPk(req.params.id, {
+      // be sure to include its associated Product data
+      include: { 
+        model: Product,
+      },
+    });
+    if(!tagsData) {
+      res.status(404).json({ message: 'No tags with this specific id!'});
+      return;
+    }
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const tagsData = await Tag.create(req.body);
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagsData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagsData) {
+      res.status(404).json({ message: "No tags with this specific id to update!" });
+      return;
+    }
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagsData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagsData) {
+      res.status(404).json({ message: "No tags with this specific id to delete!"})
+      return;
+    }
+    res.status(200).json(tagsData);
+  } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Product data
-});
-
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
-
-router.post('/', (req, res) => {
-  // create a new tag
-});
-
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
 });
 
 module.exports = router;
