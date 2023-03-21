76e8ea06100cd63a6e42a9052604b1367bfa2714
sdfields <spencerdfields@gmail.com>
2023-03-20 21:08:51 -0500

    updated readme just needs video

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
+// Requirements
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// Connection to sequelize through env file
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
